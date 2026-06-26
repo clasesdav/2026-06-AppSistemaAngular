08c086c11f0dba0cfd9d947f4c566da62e670799
Docente-Lab <davidserrudoclases@gmail.com>
2026-06-26 19:29:43 -0400

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Bienvenida } from './bienvenida/bienvenida';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Bienvenida],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('appSistema');
+
+   nombre = 'David';
+cursos = signal(['Angular', 'TypeScript', 'HTML', 'CSS']);
 }
